3e68655eeaefff23038bdeb6abb83b49d296d2d2
Michael McKay <mckaymic@us.ibm.com>
2016-04-19 23:18:36 -0400

    Bumped version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ibmcloud:accounts-bluemix',
   summary: "Login service for Bluemix accounts",
-  version: "1.0.1",
+  version: "1.0.2",
   git: 'https://github.com/oneibmcloud/accounts-bluemix.git'
 });
 
