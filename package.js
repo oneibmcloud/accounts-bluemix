1a2f44f1460eaa8606ab6cbc6130b77235c677c2
Michael McKay <mckaymic@us.ibm.com>
2016-04-19 23:16:54 -0400

    Updated bluemix dependency

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.use('accounts-base@1.0.0', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth@1.0.0', ['client', 'server']);
-  api.use('ibmcloud:bluemix@1.0.0', ['client', 'server']);
+  api.use('ibmcloud:bluemix@1.0.2', ['client', 'server']);
 
   api.addFiles('bluemix_login_button.css', 'client');
   api.addFiles("bluemix.js");
